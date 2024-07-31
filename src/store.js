4b75f212859b3a5b38aa347001506d5812db7c4f
VIGNESHKUMAR P <124986963+vigneshkumar316@users.noreply.github.com>
2024-07-31 09:39:16 +0530

    Delete src directory

--- src/store.js
+++
@@ -1,9 +0,0 @@
-// src/store.js
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './slices/authSlice'; // Adjust the path if needed
-
-export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-  },
-});
